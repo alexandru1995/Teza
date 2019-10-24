13072f8ccfda2114be0337d47b6df27e9a7d01c0
Alex <marii.alexandru95@gmail.com>
2019-10-25 00:19:57 +0300

    Create template for navbar and login card

---
+++ MAuthen.Client/ClientApp/src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+
+
+
+@NgModule({
+  declarations: [NavbarComponent,  CardComponent],
+  exports:[
+    NavbarComponent,
+    CardComponent
+  ],
+  imports: [
+    CommonModule
+  ]  
+})
+export class ComponentsModule { }
